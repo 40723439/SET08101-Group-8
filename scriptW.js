882bb927c21aec12470ea0174ad4a30b99fbad89
Wiktor Lewandowski <40723439@live.napier.ac.uk>
2025-04-24 16:31:01 +0100

    2.8

--- scriptW.js
+++ scriptW.js
@@ -118,29 +118,31 @@ function renderScene(sceneId) {
     });
 }
     
-// Sidebar
-document.getElementById("menuToggle").onclick = function () {
-    const sidebar = document.getElementById("mySidebar");
-    const main = document.getElementById("main");
-    const isOpen = sidebar.style.width === "250px";
-    sidebar.style.width = isOpen ? "0" : "250px";
-    main.style.marginLeft = isOpen ? "0" : "250px";
-};
-// Music
-document.getElementById("musicMenuBtn").onclick = function (e) {
-    e.preventDefault();
-    const credits = document.getElementById("musicCredits");
-    credits.style.display = credits.style.display === "block" ? "none" : "block";
-};
-// Music Credits
-document.getElementById("muteToggle").onclick = function () {
-    audioMuted = !audioMuted;
-    const audio = document.getElementById("sceneAudio");
-    audio.muted = audioMuted;
-    this.innerText = audioMuted ? "🔊 Unmute" : "🔇 Mute";
-};
 
-// Run the function to fetch scenes from JSON
+
+// Run the function to fetch scenes from JSON and load menu's etc
 window.onload = () => {
     loadScenes();
+
+    // Sidebar
+    document.getElementById("menuToggle").onclick = function () {
+        const sidebar = document.getElementById("mySidebar");
+        const main = document.getElementById("main");
+        const isOpen = sidebar.style.width === "250px";
+        sidebar.style.width = isOpen ? "0" : "250px";
+        main.style.marginLeft = isOpen ? "0" : "250px";
+    };
+    // Music
+    document.getElementById("musicMenuBtn").onclick = function (e) {
+        e.preventDefault();
+        const credits = document.getElementById("musicCredits");
+        credits.style.display = credits.style.display === "block" ? "none" : "block";
+    };
+    // Music Credits
+    document.getElementById("muteToggle").onclick = function () {
+        audioMuted = !audioMuted;
+        const audio = document.getElementById("sceneAudio");
+        audio.muted = audioMuted;
+        this.innerText = audioMuted ? "🔊 Unmute" : "🔇 Mute";
+    };
 };
